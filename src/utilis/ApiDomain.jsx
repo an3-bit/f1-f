53418c0b00b2d87f9437c7029fc0d77c488fbb4c
an3-bit <andrewmogambi12@gmail.com>
2025-05-26 23:10:27 +0300

    .

--- src/utilis/ApiDomain.jsx
+++ src/utilis/ApiDomain.jsx
@@ -1,4 +1,4 @@
 // export const API_DOMAIN =  'http://localhost:8081/api';
-export const API_DOMAIN =  'http://68.183.84.153:8081/api';
+export const API_DOMAIN =  'https://f1-backend-t9zk.onrender.com/api';
 
 export { API_DOMAIN as ApiDomain };
